2da11101f0a39c45d7eff69ed38742d10d300518
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-16 08:47:52 -0800

    Update FoodTruckA.java

--- FoodTruckA.java
+++ FoodTruckA.java
@@ -1,3 +1,31 @@
 package foodtrucka;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodTruckA {
+    private List<Food> inventory = new ArrayList<>();
+    private List<Food> inventory2 = inventory;
+    
+    public void addFood(int type){
+        this.inventory.add(new Food(type));
+    }
+    public void removeFood(int type){
+        this.inventory.remove(new Food(type));
+    }
+    public void addFoods(int amount, int type){
+        for (int i = 0; i < amount; i++){
+            this.inventory.add(new Food(type));
+        }
+    }
+    public void removeFoods(int amount, int type){
+        for (int i = 0; i < amount; i++){
+            this.inventory.remove(new Food(type));
+        }
+    }
+    public List<Food> countFood(int type){
+        return this.inventory2;
+    }
+    public static void removeAll(int type){}
 }
+
