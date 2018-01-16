ffa72a33f54e3826adef58df36d6256b593ccaa8
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-16 08:48:05 -0800

    Update Cashier.java

--- Cashier.java
+++ Cashier.java
@@ -4,71 +4,9 @@ public class Cashier {
     private static Scanner input = new Scanner(System.in);
     private static FoodTruckA foodtruck = new FoodTruckA();
     
-    public static void placeOrder() {
-        boolean ordering = true;
-
-            while (ordering){
-                System.out.println("What would you like to order?");
-                System.out.println("1) Hamburger");
-                System.out.println("2) Pizza");
-                System.out.println("3) Cheeseburger");
-                System.out.println("4) Wings");
-                System.out.println("5) Cheesie Boy");
-                System.out.println("6) Water");
-                System.out.println("7) Soda");
-                String choice = input.next();
-                switch(choice){
-                case "1":
-                    System.out.println("Your food will cost $" + Food.hamburger);
-                    System.out.println();
-                break;
-                
-                case "2":
-                    System.out.println("Your food will cost $" + Food.pizza);
-                    System.out.println();
-                break;
-                
-                case "3":
-                    System.out.println("Your food will cost $" + Food.cheeseburger);
-                    System.out.println();
-                break;
-                
-                case "4":
-                    System.out.println("Your food will cost $" + Food.wings);
-                    System.out.println();
-                break;
-                
-                case "5":
-                    System.out.println("Your food will cost $" + Food.cheesieboy);
-                break;
-                
-                case "6":
-                    System.out.println("Your drink will cost $" + Food.water);
-                    System.out.println();
-                break;
-                 
-                case "7" :
-                    System.out.println("Your drink will cost $" + Food.soda);
-                    System.out.println();
-                break;
-                 }
-            System.out.println("Would you like to order anything else? 1) Yes 2) No");
-            int newChoice = input.nextInt();
-            if (newChoice == 1){
-                ordering = true;
-            }
-            if (newChoice == 2){
-                ordering = false;
-            }else if (newChoice >= 3 || newChoice <1){
-                System.out.println("That choice does not exist!");
-            }
-        }
-            
-    }
-    public static void takeMoney(int type){}
+    
+    public static void placeOrder() { }
     public static void checkMoney(){}
     public static void giveFood(){}
-    public static void main(String[] args){
-        placeOrder();
-    }
+    public static void main(String[] args){}
 }
