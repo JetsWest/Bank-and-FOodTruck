b3a192bb38cf6475cc9fee2657ea714b6b4dd8b3
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-16 08:50:37 -0800

    Update Food.java

--- Food.java
+++ Food.java
@@ -3,42 +3,25 @@ package foodtrucka;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Food {
-    private List<Food> foods = new ArrayList<>();
-
+public class FoodTruckA {
+    private List<Food> inventory = new ArrayList<>();
     
-    public static final int unknown = 0;
-    public static final int hamburger = 10;
-    public static final int wings = 5;
-    public static final int cheeseburger = 12;
-    public static final int pizza = 15;
-    public static final int cheesieboy = 25;
-    public static final int water = 1;
-    public static final int soda = 2;
-    
-    private double price;
-    private int type;
-    
-    
-    public Food(int type){
-        if(type == pizza){
-            this.price = 15.00;
-        }else if(type == wings){
-            this.price = 5.00;
-        }else if(type == cheesieboy){
-            this.price = 25.00;
-        }else if(type == hamburger){
-            this.price = 10.00;
-        }else if(type == cheeseburger){
-            this.price = 12.00;
-        }else if(type == water){
-            this.price = 1.00;
-        }else if(type == soda){
-            this.price = 2.00;
-        }else{
-            this.price = 0.00;
-            this.type = unknown;
-        }
-        this.type = type;
+    public void addFood(int type){
+        this.inventory.add(new Food(type));
     }
+    public void removeFood(int type){
+        this.inventory.remove(new Food(type));
+    }
+    public void addFoods(int amount, int type){
+        for (int i = 0; i < amount; i++){
+            this.inventory.add(new Food(type));
+        }
+    }
+    public void removeFoods(int amount, int type){
+        for (int i = 0; i < amount; i++){
+            this.inventory.remove(new Food(type));
+        }
+    }
+    public int countFood(int type){}
+    public static void removeAll(int type){}
 }
