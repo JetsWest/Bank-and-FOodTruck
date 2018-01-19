8add6c034d67ac55aa0b63d36e8beb756d41e1aa
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-18 16:41:51 -0800

    Update Food.java

--- Food.java
+++ Food.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Food {
     public static final int UNKNOWN = -1;
     public static final int HAMBURGER = 3;
-    public static final int CHEESEBURGER = 3;
+    public static final int CHEESEBURGER = 5;
     public static final int HOTDOG = 4;
-    public static final int SODA = 4;
+    public static final int SODA = 7;
     public static final int CHEESIEBOY = 200;
     public static final int WATER = 10;
     public static final int YEET = 2;
@@ -21,11 +21,11 @@ public class Food {
         if(type==HAMBURGER){
             this.price = 3.00;
         }else if(type==CHEESEBURGER){
-            this.price = 3.50;
+            this.price = 5.00;
         }else if(type==HOTDOG){
             this.price = 4.00;
         }else if(type==SODA){
-            this.price = 4.00;
+            this.price = 7.00;
         }else if(type==CHEESIEBOY){
             this.price = 200.00;
         }else if(type==WATER){
@@ -37,7 +37,7 @@ public class Food {
             this.type = UNKNOWN;
         }
     }
-    public double getPrice(int type){
+    public double getPrice(){
         return this.price;
     }
     public double getType(){
