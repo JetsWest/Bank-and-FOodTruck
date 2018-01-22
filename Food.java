2dfd937a619020302ea0d51ad0d0bc38adfdaa0f
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-22 08:49:48 -0800

    Update Food.java

--- Food.java
+++ Food.java
@@ -20,18 +20,25 @@ public class Food {
     public Food(int type){
         if(type==HAMBURGER){
             this.price = 3.00;
+            this.type = 1;
         }else if(type==CHEESEBURGER){
             this.price = 5.00;
+            this.type = 2;
         }else if(type==HOTDOG){
             this.price = 4.00;
+            this.type = 3;
         }else if(type==SODA){
             this.price = 7.00;
+            this.type = 4;
         }else if(type==CHEESIEBOY){
             this.price = 200.00;
+            this.type = 5;
         }else if(type==WATER){
             this.price =10.00;
+            this.type = 6;
         }else if(type==YEET){
             this.price = 2.00;
+            this.type = 7;
         }else{
             this.price = 0.00;
             this.type = UNKNOWN;
@@ -40,7 +47,7 @@ public class Food {
     public double getPrice(){
         return this.price;
     }
-    public double getType(){
+    public int getType(){
         return this.type;
     }
 }
