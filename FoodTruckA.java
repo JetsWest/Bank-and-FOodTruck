7ecf53e0e66705c2d81c1bc819f18b07fcc7534f
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-23 08:48:46 -0800

    Update FoodTruckA.java

--- FoodTruckA.java
+++ FoodTruckA.java
@@ -4,29 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FoodTruckA {
-    private final List<Food> inventory = new ArrayList<>();
+    private List<Food> inventory = new ArrayList<>();
     private double balance = 100000.00;
+    private double markup;
+    public static int typer;
+    public static Food food = new Food(typer);
     
-    public void startingInv(){
-        addFoods(10, Food.HAMBURGER);
-        addFoods(10, Food.CHEESEBURGER);
-        addFoods(10, Food.HOTDOG);
-        addFoods(10, Food.SODA);
-        addFoods(10, Food.CHEESIEBOY);
-        addFoods(10, Food.WATER);
-        addFoods(10, Food.YEET);
+    public FoodTruckA(){
+    this.inventory = new ArrayList<>();
+    this.balance = 10000000.00;
+    this.markup = 1;
     }
-    public void addFood(int type){
-        this.inventory.add(new Food(type));
-    }
-    public void removeFood(int type){
-        this.inventory.remove(new Food(type));
+    public ArrayList<Food> restock(ArrayList<Food> order){
+        ArrayList<Food> result = new ArrayList<>();
+        while(this.balance >0 && order.size() >0){
+            this.balance -= order.get(0).getPrice();
+            result.add(order.get(0));
+            this.inventory.add(order.remove(0));
+        }
+        return result;
     }
     public int addFoods(int amount, int type){
         for (int i = 0; i < amount; i++){
             this.inventory.add(new Food(type));
         }
         return checkCertainFood(type);
+    }
+    public ArrayList<Food> fulfillOrder(ArrayList<Food> order){
+        ArrayList<Food> result = new ArrayList<>();
+        int current = 0;
+        while(current < order.size() && this.inventory.size()>0){
+            if (this.inventory.contains(order.get(current))){
+            result.add(order.get(current));
+            this.balance += order.get(current).getPrice()*this.markup;
+            inventory.remove(order.remove(current));
+        }else{
+            current++;
+            }
+       }
+        return result;
     }
     public void removeFoods(int amount, int type){
         for (int i = 0; i < amount; i++){
@@ -44,22 +60,17 @@ public class FoodTruckA {
         }
         return count;
     }
-
-    public Food[] checkInventory(){
-        return this.inventory.toArray(new Food[] {});
-    }
-    public void removeAll(int type){
-        inventory.clear();
-    }
+    
     public double checkBalance(){
         return this.balance;
     }
-    public void takeMoney(int price){
+    public void takeMoney(double price){
         this.balance += price;
     }
-    public double setMarkup(double set){
-        double markup = set;
+    public double setMarkup(double fraction){
+        this.markup = fraction; 
         return markup;
     }
+    
 
 }
