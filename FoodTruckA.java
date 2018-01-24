e75b4e628037d4a651da9b3e0d7531d095db2ba5
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-23 16:53:19 -0800

    Update FoodTruckA.java

--- FoodTruckA.java
+++ FoodTruckA.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class FoodTruckA {
     private List<Food> inventory = new ArrayList<>();
-    private double balance = 100000.00;
+    private double balance;
     private double markup;
     public static int typer;
     public static Food food = new Food(typer);
     
     public FoodTruckA(){
     this.inventory = new ArrayList<>();
-    this.balance = 10000000.00;
+    this.balance = 100000.00;
     this.markup = 1;
     }
     public ArrayList<Food> restock(ArrayList<Food> order){
         ArrayList<Food> result = new ArrayList<>();
-        while(this.balance >0 && order.size() >0){
+        while(this.balance > 0 && order.size() > 0){
             this.balance -= order.get(0).getPrice();
             result.add(order.get(0));
             this.inventory.add(order.remove(0));
@@ -27,6 +27,7 @@ public class FoodTruckA {
     public int addFoods(int amount, int type){
         for (int i = 0; i < amount; i++){
             this.inventory.add(new Food(type));
+            this.balance -= food.prices[type];
         }
         return checkCertainFood(type);
     }
@@ -37,7 +38,7 @@ public class FoodTruckA {
             if (this.inventory.contains(order.get(current))){
             result.add(order.get(current));
             this.balance += order.get(current).getPrice()*this.markup;
-            inventory.remove(order.remove(current));
+            this.inventory.remove(order.remove(current));
         }else{
             current++;
             }
@@ -70,6 +71,12 @@ public class FoodTruckA {
     public double setMarkup(double fraction){
         this.markup = fraction; 
         return markup;
+    }
+    public double getMarkup(){
+        return markup;
+    }
+    public double checkPrice(){
+        return markup*food.getPrice();
     }
     
 
