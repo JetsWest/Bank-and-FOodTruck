4f9b8376ce887886ed45bbe1004cbe5db4fbf0cc
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-01-23 16:52:58 -0800

    Update Food.java

--- Food.java
+++ Food.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class Food {
     public static final int UNKNOWN = -1;
-    public static final int HAMBURGER = 3;
-    public static final int CHEESEBURGER = 5;
-    public static final int HOTDOG = 4;
-    public static final int SODA = 7;
-    public static final int CHEESIEBOY = 200;
-    public static final int WATER = 10;
-    public static final int YEET = 2;
-    private static final double[] prices = new double[]{3.0,5.0,4.0,7.0,200.0,10.0,2.0};
+    public static final int HAMBURGER = 1;
+    public static final int CHEESEBURGER = 2;
+    public static final int HOTDOG = 3;
+    public static final int SODA = 4;
+    public static final int CHEESIEBOY = 5;
+    public static final int WATER = 6;
+    public static final int YEET = 7;
+    public static final double[] prices = new double[]{3.0,5.0,4.0,7.0,200.0,10.0,2.0};
     private double price;
     private int type;
     public int balance;
@@ -34,7 +34,7 @@ public class Food {
             this.price = 200.00;
             this.type = 5;
         }else if(type==WATER){
-            this.price =10.00;
+            this.price=10.00;
             this.type = 6;
         }else if(type==YEET){
             this.price = 2.00;
